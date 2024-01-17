73dc3be0cf7422e5b6fe91bb2d35fc5368e3828e
陈亮琦 <chenliangqi@chenliangqideMacBook-Pro.local>
2024-01-17 11:40:10 +0800

    update BST

---
+++ lecture_10.2/BST.java
@@ -0,0 +1,17 @@
+//import java.security.Key;
+
+private class BST<Key> {
+    private Key key;
+    private BST left;
+    private BST right;
+
+    public BST(Key key, BST left, BST right) {
+        this.key = key;
+        this.left = left;
+        this.right = right;
+    }
+
+    public BST(Key key) {
+        this.key = key;
+    }
+}
