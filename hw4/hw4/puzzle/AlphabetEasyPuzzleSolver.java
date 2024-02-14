542913072eb8e6de6bfc3c43956752b70e314fbb
chenliangqi <1523085763@qq.com>
2024-02-14 20:59:42 +0800

    hw4

--- hw4/hw4/puzzle/AlphabetEasyPuzzleSolver.java
+++ hw4/hw4/puzzle/AlphabetEasyPuzzleSolver.java
@@ -1,4 +1,5 @@
 package hw4.puzzle;
+
 import edu.princeton.cs.algs4.StdOut;
 
 public class AlphabetEasyPuzzleSolver {
@@ -6,15 +7,15 @@ public class AlphabetEasyPuzzleSolver {
      * Test routine for your Solver class. Uncomment and run to test
      * your basic functionality.
      **********************************************************************/
-    /*public static void main(String[] args) {
-        char start = 'w';
-
-        AlphabetEasyPuzzle startState = new AlphabetEasyPuzzle(start);
-        Solver solver = new Solver(startState);
-
-        StdOut.println("Minimum number of moves = " + solver.moves());
-        for (WorldState ws : solver.solution()) {
-            StdOut.println(ws);
-        }
-    }*/
+//    public static void main(String[] args) {
+//        char start = 'w';
+//
+//        AlphabetEasyPuzzle startState = new AlphabetEasyPuzzle(start);
+//        Solver solver = new Solver(startState);
+//
+//        StdOut.println("Minimum number of moves = " + solver.moves());
+//        for (WorldState ws : solver.solution()) {
+//            StdOut.println(ws);
+//        }
+//    }
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,59 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.HashSet;
+
+public class Solver {
+    private Stack<WorldState> solutions = new Stack<>();
+    private MinPQ<SearchNode> MP = new MinPQ<>();
+    private HashSet<WorldState> marked = new HashSet<>();
+
+    public Solver(WorldState initial) {
+        MP.insert(new SearchNode(initial, 0, null));
+        marked.add(initial);
+        while (!MP.min().word.isGoal()) {
+            SearchNode min = MP.delMin();
+            solutions.push(min.word);
+            for (WorldState w : min.word.neighbors()) {
+//                if (!marked.contains(w)) {
+//                    marked.add(w);
+//                    MP.insert(new SearchNode(w, min.moves + 1, min));
+//                }
+                if (min.previousNode == null || !w.equals(min.previousNode.word)) {
+                    marked.add(w);
+                    MP.insert(new SearchNode(w, min.moves + 1, min));
+                }
+            }
+        }
+
+    }
+
+    public int moves() {
+        return MP.min().moves;
+    }
+
+    public Iterable<WorldState> solution() {
+        return solutions;
+    }
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private WorldState word;
+        private int moves;
+        private SearchNode previousNode;
+        private int priority;
+
+        public SearchNode(WorldState initial, int moves, SearchNode previousNode) {
+            this.word = initial;
+            this.moves = moves;
+            this.previousNode = previousNode;
+            this.priority = moves + initial.estimatedDistanceToGoal();
+        }
+
+        @Override
+        public int compareTo(SearchNode o) {
+            return this.priority - o.priority;
+        }
+    }
+}
